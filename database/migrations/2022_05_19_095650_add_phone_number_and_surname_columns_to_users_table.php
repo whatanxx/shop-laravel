f140379d02c3e619dc589fd7c85aa75b09789a30
whatanx <whatanxx@gmail.com>
2022-05-19 12:15:27 +0200

    #12 Dodanie dodatkowy pól do modelu użytkownika - pierwsza migracja

--- database/migrations/2022_05_19_095650_add_phone_number_and_surname_columns_to_users_table.php
+++ database/migrations/2022_05_19_095650_add_phone_number_and_surname_columns_to_users_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('surname')->after('name')->nullable();
+            $table->string('phone_number', 20)->after('surname')->nullable();
         });
     }
 
@@ -31,6 +26,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['surname', 'phone_number']);
+        });
     }
 };
